60ef2a95c37c8576c29ea6bb4e9e402c1d970701
0-kw-h <63191778+0-kw-h@users.noreply.github.com>
2021-03-31 03:53:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,17 @@
 import discord
+import asyncio
 import os
 import random
+import botFunction.functions as f
 import traceback
-
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+client = discord.Client()
 
+@client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/sisotofu':
         await message.channel.send('くそがよぉ')
         
