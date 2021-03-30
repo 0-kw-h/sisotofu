07bf4be9eece2fa0718235a245fdfc2a953e9817
0-kw-h <63191778+0-kw-h@users.noreply.github.com>
2021-03-31 04:50:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,17 +9,22 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = 'このBOTはしそとうふさんの善意により運営されています。感謝を欠かすことの無いよう、常々意識をお願いします。'
     await ctx.send(error_msg)
 
 @bot.command()
 async def yukka(ctx):
-    yukkarep = ['ゆっかさんだぁ', '殴らないで……', 'ゆっかさん寝た？','ゆっかさん寝てください','ゆっかさん！','ゆっかさん…いじめないで…いたいょ…','ゆっかさんにいたずらしたいなあ','しそはDVされてる側なんです','ごめんなさい……','ゆっかさん？？？？？？','おはようございますゆっかさん']
+    yukkarep = ['ゆっかさんだぁ', '殴らないで……', 'ゆっかさん寝た？','ゆっかさん寝てください','ゆっかさん！','ゆっかさん…いじめないで…いたいょ…','ゆっかさんにいたずらしたいなあ','しそはDVされてる側なんです','ごめんなさい……','ゆっかさん？？？？？？','おはようございますゆっかさん','ゆっかさんもしかしてしそ以外にもDVしてるんですか…？','ゆっかさんとしそ不仲説；；']
     ya = random.choice(yukkarep)
     await ctx.send(ya)
 
 @bot.command()
 async def dice(ctx):
     await ctx.send(random.randint(0,100))
+
+@bot.command()
+async def siso(ctx):
+    sisorep = ['くそがよぉ','泣いた','はらたつ']
+    await ctx(sa)
 
 bot.run(token)
