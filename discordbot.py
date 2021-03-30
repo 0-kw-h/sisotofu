208e6ca17e7cab1ef4e9034af28e860473e45bd9
0-kw-h <63191778+0-kw-h@users.noreply.github.com>
2021-03-31 03:45:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,24 +6,14 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send('どうして……')
-
-@bot.command()
-async def sisotofu(ctx):
-    await ctx.send('くそがよぉ')
-
-@bot.command()
-async def DV(ctx):
-    await ctx.send('殴らないで……')
-
-@bot.command()
-async def yukka(ctx):
-    await ctx.send('ゆっかさんだー')
-
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/sisotofu':
+        await message.channel.send('くそがよぉ')
+        
 @bot.command()
 async def dice(ctx):
     await ctx.send(random.randint(0,100))
