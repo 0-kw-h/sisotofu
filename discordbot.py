33169e116261b1d617db3543fa9cd576bbc8e56b
0-kw-h <63191778+0-kw-h@users.noreply.github.com>
2021-03-31 04:42:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,9 +14,12 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def yukka(ctx):
-    yukkarep = ['ゆっかさんだー', '殴らないで……', 'ゆっかさん寝ましたか？']
+    yukkarep = ['ゆっかさんだぁ', '殴らないで……', 'ゆっかさん寝た？','ゆっかさん寝てください','ゆっかさん！','ゆっかさん…いじめないで…いたいょ…','ゆっかさんにいたずらしたいなあ','しそはDVされてる側なんです','ごめんなさい……','ゆっかさん？？？？？？','おはようございますゆっかさん']
     ya = random.choice(yukkarep)
     await ctx.send(ya)
 
+@bot.command()
+async def dice(ctx):
+    await ctx.send(random.randint(0,100))
 
 bot.run(token)
