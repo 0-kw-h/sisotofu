9a41804db20d0e8878aba9b267c2f220e39350c8
0-kw-h <63191778+0-kw-h@users.noreply.github.com>
2021-03-31 04:30:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,12 +4,19 @@ import traceback
 import random
 
 bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN]
+token = os.environ['DISCORD_BOT_TOKEN’]
 
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+
+@bot.command()
+async def yukka(ctx):
+    yukkarep = ['ゆっかさんだー', '殴らないで……', 'ゆっかさん寝ましたか？']
+    ya = random.choice(yukkarep)
+    await ctx.send(ya)
+
 
 bot.run(token)
